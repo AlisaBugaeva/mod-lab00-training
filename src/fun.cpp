676e3e71ee1f0f85f1534fcef24eccb651ce0e23
AlisaBugaeva <92516286+AlisaBugaeva@users.noreply.github.com>
2022-03-04 17:52:56 +0300

    new

--- src/fun.cpp
+++ src/fun.cpp
@@ -4,8 +4,8 @@
 int64_t power(int64_t x, uint16_t n) {
     int64_t ans =x;
     int i;
-    for(i=1;i<n;i++){
-        ans=ans*x;
+    for ( i = 1; i < n; i++){
+        ans = ans * x;
     }
     return ans;
 }
