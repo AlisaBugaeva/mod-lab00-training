7757952502ac7434f861d0fac15910b19be9d494
AlisaBugaeva <92516286+AlisaBugaeva@users.noreply.github.com>
2022-03-04 17:59:59 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,11 +1,10 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
-int64_t power(int64_t x, uint16_t n) 
-{
+int64_t power(int64_t x, uint16_t n) {
     int64_t ans = x;
     int i;
-    for ( i = 1; i < n; i++ ){
+    for ( i = 1; i < n; i++ ) {
         ans = ans * x;
     }
     return ans;
